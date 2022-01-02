793931f0d05deaeba0781aa47c55d9db6d25c49a
Daniela Tomás <up202004946@edu.fc.up.pt>
2022-01-02 12:52:59 +0000

    New class called Menu

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -16,39 +19,27 @@ import java.net.URL;
 
 public class Game {
     private Screen screen;
-    private final int width;  //terminal width
-    private final int height; //terminal height
+    private int width;  //terminal width
+    private int height; //terminal height
     private int fontSize;
+    //private Field field;
+    private Menu menu;
+    private TextGraphics graphics;
 
-    Game(int width, int height) {
+    Game(int width, int height) throws IOException, URISyntaxException, FontFormatException {
         this.width = width;
         this.height = height;
         fontSize = 25;
-    }
-    private Font loadFont() throws IOException, FontFormatException, URISyntaxException {
-        URL resource = getClass().getClassLoader().getResource("square.ttf");
-        return Font.createFont(Font.TRUETYPE_FONT, new File(resource.toURI()));
-    }
-    private void initializeScreen(Terminal terminal) throws IOException {
-        screen = new TerminalScreen(terminal);
-        screen.setCursorPosition(null);   // we don't need a cursor
-        screen.startScreen();             // screens must be started
-        screen.doResizeIfNecessary();     // resize screen if necessary
-        screen.refresh();
-    }
-    public void run() throws IOException, FontFormatException, URISyntaxException {
+        //field = new Field();
         Font font = loadFont();
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         ge.registerFont(font);
-
         DefaultTerminalFactory factory = new DefaultTerminalFactory();
-
         Font loadedFont = font.deriveFont(Font.PLAIN, fontSize);
         AWTTerminalFontConfiguration fontConfig = AWTTerminalFontConfiguration.newInstance(loadedFont);
         factory.setTerminalEmulatorFontConfiguration(fontConfig);
         factory.setForceAWTOverSwing(true);
         factory.setInitialTerminalSize(new TerminalSize(width, height));
-
         Terminal terminal = factory.createTerminal();
         ((AWTTerminalFrame)terminal).addWindowListener(new WindowAdapter() {
             @Override
@@ -57,5 +48,65 @@ public class Game {
             }
         });
         initializeScreen(terminal);
+        graphics = screen.newTextGraphics();
+        screen.clear();
+        menu = new Menu(graphics);
+        screen.refresh();
+    }
+
+    private Font loadFont() throws IOException, FontFormatException, URISyntaxException {
+        URL resource = getClass().getClassLoader().getResource("square.ttf");
+        return Font.createFont(Font.TRUETYPE_FONT, new File(resource.toURI()));
+    }
+
+    private void initializeScreen(Terminal terminal) throws IOException {
+        screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null);   // we don't need a cursor
+        screen.startScreen();             // screens must be started
+        screen.doResizeIfNecessary();     // resize screen if necessary
+        screen.refresh();
+    }
+
+    public void run() throws IOException {
+        while (true) {
+            KeyStroke key = screen.readInput();
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') screen.close();
+            if (key.getKeyType() == KeyType.EOF) break;
+            System.out.println(key);
+            processKey(key);
+            screen.refresh();
+        }
+    }
+
+    private void processKey(KeyStroke key) throws IOException {
+        if(menu.isSelected())
+           menu.processKey(key, graphics);
+        if(!menu.isSelected())
+           if(menu.getSelected() == 1) {
+                screen.clear();
+            }
+            else if(menu.getSelected() == 2) {
+                screen.close();
+            }
+        //field.processKey(key);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public int getFontSize() {
+        return fontSize;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public void setFontSize(int fontSize) {
+        this.fontSize= fontSize;
     }
 }
---
+++ src/main/java/Menu.java
@@ -0,0 +1,66 @@
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+
+public class Menu {
+    private int selected;
+    private boolean isSelected;
+
+    Menu(TextGraphics graphics) {
+        selected = 1;
+        isSelected = true;
+        draw(graphics);
+    }
+
+    void processKey(KeyStroke key,TextGraphics graphics) {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#DAA520"));
+        switch(key.getKeyType()) {
+            case ArrowDown:
+                if(getSelected() == 1) selectDown(graphics);
+                break;
+            case ArrowUp:
+                if(getSelected() == 2) selectUp(graphics);
+                break;
+            case Enter:
+                isSelected = false;
+                break;
+        }
+    }
+
+    private void selectDown(TextGraphics graphics) {
+        selected = 2;
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#000000"));
+        graphics.fillRectangle(new TerminalPosition(25, 9), new TerminalSize(7, 3), ' ');
+        drawString(graphics,26,10,"Start");
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#EEE8AA"));
+        graphics.fillRectangle(new TerminalPosition(25, 14), new TerminalSize(7, 3), ' ');
+        drawString(graphics,26,15,"Exit");
+    }
+    private void selectUp(TextGraphics graphics) {
+        selected = 1;
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#EEE8AA"));
+        graphics.fillRectangle(new TerminalPosition(25, 9), new TerminalSize(7, 3), ' ');
+        drawString(graphics,26,10,"Start");
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#000000"));
+        graphics.fillRectangle(new TerminalPosition(25, 14), new TerminalSize(7, 3), ' ');
+        drawString(graphics,26,15,"Exit");
+    }
+    public void draw(TextGraphics graphics) {
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.setForegroundColor(TextColor.Factory.fromString("#DAA520"));
+        drawString(graphics,21,5,"The Sheep's Escape");
+        selectUp(graphics);
+    }
+    private void drawString(TextGraphics graphics, int w, int h, String s) {
+        graphics.putString(new TerminalPosition(w, h), s);
+    }
+    public int getSelected() {
+        return selected;
+    }
+    public boolean isSelected() {
+        return isSelected;
+    }
+}
