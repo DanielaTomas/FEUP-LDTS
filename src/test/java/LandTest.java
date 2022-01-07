9e41fd8de5877b37fedf03f63336addc19e4942c
Daniela Tomás <up202004946@edu.fc.up.pt>
2022-01-07 00:20:25 +0000

    Test for Land created

---
+++ src/test/java/LandTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LandTest {
+    @Test
+    public void landTest() {
+        Land land = new Land(10,10);
+
+        assertEquals(new Position(10,10), land.getPosition());
+        land.setPosition(new Position(10,10));
+        assertEquals(new Position(10,10), land.getPosition());
+    }
+}
