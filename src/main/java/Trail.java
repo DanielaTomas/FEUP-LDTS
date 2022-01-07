34eee75382ef094278cea5d3083c082916dd3916
Daniela Tomás <up202004946@edu.fc.up.pt>
2022-01-07 19:20:06 +0000

    Add code that passes on TrailTest

---
+++ src/main/java/Trail.java
@@ -0,0 +1,28 @@
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+
+public class Trail extends Element implements LandTransformer {
+        int x;
+        int y;
+        Trail(int x, int y) {
+            super(x, y);
+            this.x = x;
+            this.y = y;
+        }
+
+    @Override
+    public void draw (TextGraphics graphics){
+        graphics.setForegroundColor(TextColor.Factory.fromString("#008000"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(getPosition().getX(), getPosition().getY()),"~");
+    }
+
+    @Override
+    public void execute(Land land) {
+        land.setType(this.x,this.y,2);
+    }
+}
+
