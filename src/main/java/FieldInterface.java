0544dca93ab03eeda2e547ac3c0d58aecebaa4de
Nuno Penafort <up202008405@up.pt>
2022-01-16 19:29:12 +0000

    New interface called FieldInterface

---
+++ src/main/java/FieldInterface.java
@@ -0,0 +1,26 @@
+
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import java.util.List;
+
+    interface FieldInterface {
+        void processKey(KeyStroke key, TextGraphics graphics);
+        void createTypes();
+        void createMix(LandCreate create);
+        void createTrail(LandCreate create, int px, int py);
+        void createFirst();
+        void moveSheep(Position position, int direction);
+        void moveWolves();
+        boolean checkWolfStuck(Wolf wolf);
+        void draw(TextGraphics graphics);
+        List<Wolf> createWolves();
+        boolean canSheepMove(Position position);
+        boolean canWolvesMove(Position position);
+        Sheep getSheep();
+        List<Wolf> getWolves();
+        Land getLand();
+        void setLast(int x, int y);
+        void setLastSheep(int x, int y);
+        boolean gameOver();
+        boolean win();
+    }
