8be74b96ed614ccd1e267eee68dbf670b558a5e8
Daniela Tomás <up202004946@edu.fc.up.pt>
2022-01-18 16:43:15 +0000

    Solve conflicts and Merge

--- src/main/java/FieldInterface.java
+++ src/main/java/FieldInterface.java
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 805640628545908915264c9d4f14f759ddafee10
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import java.util.List;
-
-<<<<<<< HEAD
   interface FieldInterface {
       void processKey(KeyStroke key, TextGraphics graphics);
       void createTypes();
@@ -28,26 +22,4 @@ import java.util.List;
       boolean gameOver();
       boolean win();
 }
-=======
-    interface FieldInterface {
-        void processKey(KeyStroke key, TextGraphics graphics);
-        void createTypes();
-        void createMix(LandCreate create);
-        void createTrail(LandCreate create, int px, int py);
-        void createFirst();
-        void moveSheep(Position position, int direction);
-        void moveWolves();
-        boolean checkWolfStuck(Wolf wolf);
-        void draw(TextGraphics graphics);
-        List<Wolf> createWolves();
-        boolean canSheepMove(Position position);
-        boolean canWolvesMove(Position position);
-        Sheep getSheep();
-        List<Wolf> getWolves();
-        Land getLand();
-        void setLast(int x, int y);
-        void setLastSheep(int x, int y);
-        boolean gameOver();
-        boolean win();
-    }
->>>>>>> 805640628545908915264c9d4f14f759ddafee10
+
--- src/main/java/FillInterface.java
+++ src/main/java/FillInterface.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
  interface FillInterface {
          void clearAnomalies(int a, int b);
          void insideGrass();
@@ -9,15 +8,4 @@
          void boundaryFillRec(int x, int y);
          boolean verifyPosition(int x, int y);
     }
-=======
-interface FillInterface {
-    void clearAnomalies(int a, int b);
-    void insideGrass();
-    boolean insideGrass(int x, int y);
-    boolean verifyWolves(int x, int y);
-    boolean verifyWolvesRec(int x, int y);
-    void boundaryFill(int x, int y);
-    void boundaryFillRec(int x, int y);
-    boolean verifyPosition(int x, int y);
-}
->>>>>>> 805640628545908915264c9d4f14f759ddafee10
+
